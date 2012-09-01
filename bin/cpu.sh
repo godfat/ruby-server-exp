135938d4a8d8dc70f30526393b687cadf87111b5
Lin Jen-Shin <godfat@godfat.org>
2012-09-01 08:58:25 +0800

    it seems the timeout is for all requests, not for a single one...

--- bin/cpu.sh
+++ bin/cpu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-httperf --server localhost --port 8080 --uri /cpu --rate 25 --num-conn 50 --num-call 2 --timeout 5
+httperf --server localhost --port 8080 --uri /cpu --rate 25 --num-conn 50 --num-call 2 --timeout 60
