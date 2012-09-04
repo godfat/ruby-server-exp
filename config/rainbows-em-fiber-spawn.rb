41644fc63ffa0a505eab616c1d23ead4a564d349
Lin Jen-Shin <godfat@godfat.org>
2012-09-05 05:27:08 +0800

    rainbows-em-fiber-spawn.rb: this works for pipelines too! the point:
    is to set @deferred as always and clear @deferred before writing
    response back to the client.

--- config/rainbows-em-fiber-spawn.rb
+++ config/rainbows-em-fiber-spawn.rb
@@ -21,7 +21,23 @@ end
 after_fork{ |_, _|
   class RainbowsEventMachineFiberSpawnClient < Rainbows::EventMachine::Client
     def app_call input
-      Fiber.new{ super }.resume
+      set_comm_inactivity_timeout 0
+      @env[RACK_INPUT] = input
+      @env[REMOTE_ADDR] = @_io.kgio_addr
+      @env[ASYNC_CALLBACK] = method(:write_async_response)
+      @env[ASYNC_CLOSE] = EM::DefaultDeferrable.new
+      Fiber.new{
+        status, headers, body = catch(:async) {
+          APP.call(@env.merge!(RACK_DEFAULTS))
+        }
+        if nil == status || -1 == status
+          @deferred = true
+        else
+          @deferred = nil
+          ev_write_response(status, headers, body, @hp.next?)
+        end
+      }.resume
+      @deferred = true
     end
   end
 }
